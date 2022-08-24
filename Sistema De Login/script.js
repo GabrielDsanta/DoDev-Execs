7b60f0c82228cd330bda08e4a377975ca488b0c7
GabrielDSanta <eloviveiros@hotmail.com>
2022-08-23 22:51:29 -0300

    Sistema De Login V3

--- Sistema De Login/script.js
+++ Sistema De Login/script.js
@@ -1,21 +1,31 @@
 
-
 var names = []
 var password = []
 
-var choice = true
 var index = 0
+var choice = true
+
 
 while(choice){
-    names[index] = prompt("Digite seu Usuário")
-    password[index] = prompt("Digite ssua Senha")
-
-    index++
-
-    choice = prompt("Deseja continuar ?")
-    if(choice == "Não"){
+    var choice = prompt("1 // Cadastrar, 2 // Fazer Login, 3 // Encerrar 4 //Excluir Cadastro")
+    if(choice == "1"){
+            names[index] = prompt("Cadastre um Usuário")
+            password[index] = prompt("Cadastre uma Senha")
+            index++
+    }
+    
+    if(choice == "2"){
+        var comparador = 0
+        var names2 = prompt("Qual seu usuário ?")
+        var password2 = prompt("Qual sua Senha ?")
+        for(var contador2 = 0; contador2 < names.length; contador2++){
+        }
+    }
+    
+    if(choice == "3"){
         choice = false
     }
+
 }
 
 console.log(names, password)
